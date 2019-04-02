dc6de1687fb182ec6250594cfb00082ea774f351
Nesk <nikitaselytin@yandex.ru>
2019-04-03 05:21:10 +0700

    Create class DBhelper

--- app/src/main/java/ru/nesk27/contacts/MainActivity.java
+++ app/src/main/java/ru/nesk27/contacts/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnDelete:
                 //
                 break;
-                
+
         }
     }
 }
