0417282e4b399520ed2d2096919f3ec621da7fe0
Nesk <nikitaselytin@yandex.ru>
2019-04-03 05:09:00 +0700

    Change view and class for test sql

--- app/src/main/java/ru/nesk27/contacts/MainActivity.java
+++ app/src/main/java/ru/nesk27/contacts/MainActivity.java
@@ -8,24 +8,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAdd, btnFind, btnDelete;
+    EditText lastname, firstname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnFind = (Button) findViewById(R.id.btnFind);
+        btnFind.setOnClickListener(this);
+
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(this);
+
+        lastname = (EditText) findViewById(R.id.lastname);
+        firstname = (EditText) findViewById(R.id.firstname);
     }
 
     @Override
@@ -48,5 +55,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String etFirstname = firstname.getText().toString();
+        String etLastname = lastname.getText().toString();
+
+        switch (v.getId())
+        {
+            case R.id.btnAdd:
+                //
+                break;
+            case R.id.btnFind:
+                //
+                break;
+            case R.id.btnDelete:
+                //
+                break;
+                
+        }
     }
 }
